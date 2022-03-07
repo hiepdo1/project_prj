dd49829bfb440fbe4b8dc3c0a3ed801c9f2995b9
hiepdo1 <hiepdhhe150990@fpt.edu.vn>
2022-03-08 00:03:17 +0700

    Chỉnh sửa và update database

--- database.sql
+++ database.sql
@@ -1,24 +1,21 @@
-
-create table music(
-	[musicId] int primary key,
-	[name] varchar(255),
-	[singer] varchar(255),
-	[path] varchar(255),
-);
-
-create table playList(
-	[playlistId] int primary key,
-	[name] varchar(255),
-	[musicId] int,
-	[accountId] int,
-	FOREIGN KEY (musicId) REFERENCES music(musicId),
-);
-
-create table account(
-	[accountId] int primary key,
-	[name] varchar(50),
-	[username] varchar(50),
-	[password] varchar(50),
-	playlistId int,
-	FOREIGN KEY (playlistId) REFERENCES playList(playlistId),
-);
+create database musicDb
+Create table Account(
+accountId int primary key,
+username varchar(255),
+[password] varchar(255),
+name nvarchar(255),
+)
+create table Playlist(
+playlistId int primary key,
+name nvarchar(255),
+accountID int FOREIGN KEY REFERENCES Account(accountID),
+)
+create table Music(
+musicID int primary key,
+name nvarchar(255),
+[path] varchar(255),
+)
+create table List(
+playlistID int FOREIGN KEY REFERENCES Playlist(playlistID),
+musicID int FOREIGN KEY REFERENCES Music(musicID)
+)
